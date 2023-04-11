1ad0631c019c36e14b0a3df322b6c0371a256122
KristofferMoevik <kristoffersmovik@live.no>
2023-04-11 11:10:20 +0200

    mle created and simulated signal generated

---
+++ MLE.m
@@ -0,0 +1,27 @@
+clear all
+close all
+
+F_s = 10^6;
+T = 1/F_s;
+
+f_0 = 10^5;
+omega_0 = 2*pi*f_0;
+phi = pi/8;
+A = 1;
+
+SNR_decibel = 0;
+SNR = db2mag(SNR_decibel);
+sigma = sqrt(A^2./(2*SNR));
+
+N = 513;
+n_0 = -256;
+t = 0:T:T*(N-1);
+k = [10,12,14,16,18,20];
+
+t1 = A* exp(1i*(omega_0*t + phi));
+t2 = sigma*randn(N,1);
+t3 = 1i*sigma*randn(N,1);
+
+x = A* exp(1i*(omega_0*t + phi)) + sigma*randn(1,N) + 1i*sigma*randn(1,N);
+
+plot(abs(x))
