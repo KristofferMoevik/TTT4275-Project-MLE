f32c5efebaebf1d7d52ebdbefc2638f1b2195c34
KristofferMoevik <kristoffersmovik@live.no>
2023-04-11 13:17:41 +0200

    added M for varying fft length

--- MLE.m
+++ MLE.m
@@ -11,7 +11,7 @@ omega_0 = 2*pi*f_0;
 phi = pi/8;
 A = 1;
 
-SNR_decibel = -10;
+SNR_decibel = 1;
 SNR = db2mag(SNR_decibel);
 sigma_square = A^2/2*SNR;
 sigma = sqrt(A^2./(2*SNR));
@@ -23,6 +23,7 @@ Q = N*(N-1)*(2*N-1)/6;
 n_0 = -P/N;
 t = 0:T:T*(N-1);
 k = [10,12,14,16,18,20];
+M = 2^10;
 
 %% Define signal
 
@@ -33,7 +34,8 @@ CRLB_freq = 12*sigma_square / A^2*T^2*N*(N^2-1);
 CRLB_phase = 12*sigma_square*(n_0^2*N+2*n_0*P+Q) / (A^2*N^2*(N^2-1));
 
 %% Fourier transform of x
-M = size(x,2);
+% zero pad x
+x = [x, zeros(1,M - size(x,2))];
 Y = fft(x,M);
 [val, m_star] = max(Y);
 
