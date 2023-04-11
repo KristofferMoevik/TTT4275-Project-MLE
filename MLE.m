8425c39fa05965c96f0cbee3fbf1107d75077e05
KristofferMoevik <kristoffersmovik@live.no>
2023-04-11 12:05:05 +0200

    crlb phase added pharatesis

--- MLE.m
+++ MLE.m
@@ -13,17 +13,26 @@ A = 1;
 
 SNR_decibel = 0;
 SNR = db2mag(SNR_decibel);
+sigma_square = A^2/2*SNR;
 sigma = sqrt(A^2./(2*SNR));
 
+
 N = 513;
-n_0 = -256;
+P = N*(N-1)/2;
+Q = N*(N-1)*(2*N-1)/6;
+n_0 = -P/N;
 t = 0:T:T*(N-1);
 k = [10,12,14,16,18,20];
 
-t1 = A* exp(1i*(omega_0*t + phi));
-t2 = sigma*randn(N,1);
-t3 = 1i*sigma*randn(N,1);
+%% Define signal
 
-x = A* exp(1i*(omega_0*t + phi)) + normrnd(0,sigma,N) + 1i*normrnd(0,sigma,N);
+x = A* exp(1i*(omega_0*t + phi)) + normrnd(0,sigma,1,N) + 1i*normrnd(0,sigma,1,N);
 
-plot(sigma*randn(1,N))
+%% Find CRLB
+CRLB_freq = 12*sigma_square / A^2*T^2*N*(N^2-1);
+CRLB_phase = 12*sigma_square*(n_0^2*N+2*n_0*P+Q) / (A^2*N^2*(N^2-1));
+
+
+%% Plot
+
+plot(abs(x));
