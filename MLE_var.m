9379029a5cba5417f999c92e0707d06dc24c155b
KristofferMoevik <kristoffersmovik@live.no>
2023-04-14 14:05:21 +0200

    finalized simulation

--- MLE_var.m
+++ MLE_var.m
@@ -24,37 +24,49 @@ n = n_0:1:(n_0 + N-1);
 t = 0:T:T*(N-1);
 k = [10,12,14,16,18,20];
 
-%% Define signal
-
-w_r = normrnd(0,sigma,1,N);
-w_i = 1i*normrnd(0,sigma,1,N);
-
-x = A* exp(1i*(omega_0*n*T + phi)) + w_r + w_i;
-
 %% Find CRLB
 CRLB_freq = 12*sigma_square / A^2*T^2*N*(N^2-1);
 CRLB_phase = 12*sigma_square*(n_0^2*N+2*n_0*P+Q) / (A^2*N^2*(N^2-1));
 
-%% Fourier transform of x
-
-M = 2^k(6);
-x = [x zeros(1,M-size(x,2))];
-
-Y = fft(x,M);
-%Y = Y .* [ones(1,0.5*M), zeros(1,0.5*M)];
-[val, m_star] = max(Y);
-
-%% Find estimates
-
-omega_hat = (2*pi*m_star) / (M*T);
-phi_hat = angle(exp(-1i*omega_hat*n_0*T) * val);
-omega_hat = (2*pi*m_star) / (T*M);
+sim_steps = 100;
+omega_hat = [];
+phi_hat = [];
+omega_hat = [];
 
 %% Estimation errors
 
-e_omega = omega_0 - omega_hat;
-e_phi = phi - phi_hat;
+e_omega = [];
+e_phi = [];
+for i=1:sim_steps
+    %% Define signal
+    w_r = normrnd(0,sigma,1,N);
+    w_i = 1i*normrnd(0,sigma,1,N);
+
+    x = A* exp(1i*(omega_0*n*T + phi)) + w_r + w_i;
+
+    %% Fourier transform of x
+
+    M = 2^k(6);
+    x = [x zeros(1,M-size(x,2))];
+
+    Y = fft(x,M);
+    %Y = Y .* [ones(1,0.5*M), zeros(1,0.5*M)];
+    [val, m_star] = max(Y);
+
+    %% Find estimates
+
+    omega_hat = [omega_hat; (2*pi*m_star) / (M*T)];
+    phi_hat = [phi_hat; angle(exp(-1i*omega_hat(i)*n_0*T) * val)];
+    omega_hat = [omega_hat; (2*pi*m_star) / (T*M)];
+
+    %% Estimation errors
+
+    e_omega = [e_omega; omega_0 - omega_hat(i)];
+    e_phi = [e_phi; phi - phi_hat(i)];
+end
+
+%% 
 
 %% Plot
 
-plot(abs(Y));
+plot(abs(e_omega));
