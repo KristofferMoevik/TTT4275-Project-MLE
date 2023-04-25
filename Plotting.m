9632ee8b5d44e279b4f2ec4e3f9712a8c4b67760
johannrs <johannrs@stud.ntnu.no>
2023-04-25 09:13:08 +0200

    finsihed bro

--- Plotting.m
+++ Plotting.m
@@ -12,7 +12,7 @@ n_k = length(k);
 
 %% Plotting
 
-% Frequency estimatation error
+% Frequency estimatation error variance 
 
 load('sim_data_1.mat');
 
@@ -28,11 +28,11 @@ ylabel('Variance');
 
 dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
 set(fig1, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
-%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/omega_err_var_plot');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/omega_err_var_plot2');
 
 
 
-% Phase estimation error
+% Phase estimation error variance
 
 fig2 = figure;
 semilogy(SNR_dB,phi_CRLB); hold on;  grid on;
@@ -46,19 +46,76 @@ ylabel('Variance');
 
 dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
 set(fig2, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
-%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/phi_err_var_plot');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/phi_err_var_plot2');
 
+
+% Frequency estimation opt error variance
 
 fig3 = figure;
+semilogy(SNR_dB,omega_CRLB); hold on;
+semilogy(SNR_dB, omega_MLE_error_var(:,1)); hold on;
+plot(SNR_dB, omega_MLE_error_var(:,6)); hold on;
+plot(SNR_dB, omega_opt_error_var);
+grid on;
+xlabel('SNR (dB)');
+ylabel('Variance');
+title('Variance of frequency estimate errors with tuned estimate');
+legend('CRLB','k = 10', 'k = 20', 'tuned');
+
+dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
+set(fig3, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/omega_opt_err_plot');
+
+
+% Phase estimation opt error variance
+
+fig4 = figure;
+semilogy(SNR_dB, phi_MLE_error_var(:,1)); hold on;
+plot(SNR_dB, phi_MLE_error_var(:,6)); hold on;
+plot(SNR_dB, phi_opt_error_var);
+grid on;
+xlabel('SNR (dB)');
+ylabel('Variance');
+title('Variance of phase estimate errors with tuned estimate');
+legend('k = 10', 'k = 20', 'tuned');
+
+dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
+set(fig4, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/phi_opt_err_plot');
+
+
+% Frequency estimation opt error mean
+
+fig5 = figure;
 plot(SNR_dB, omega_MLE_error_mean(:,1)); hold on;
 plot(SNR_dB, omega_MLE_error_mean(:,6)); hold on;
 plot(SNR_dB, omega_opt_error_mean);
 grid on;
 xlabel('SNR (dB)');
 ylabel('Hz');
-title('Mean of estimate errors');
-legend('k = 10', 'k = 20', 'opt');
+title('Mean of frequency estimate errors with tuned estimate');
+legend('k = 10', 'k = 20', 'tuned');
 
+dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
+set(fig5, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/omega_opt_err_mean_plot');
+
+
+% Phase estimation opt error mean
+
+fig6 = figure;
+plot(SNR_dB, phi_MLE_error_mean(:,1)); hold on;
+plot(SNR_dB, phi_MLE_error_mean(:,6)); hold on;
+plot(SNR_dB, phi_opt_error_mean);
+grid on;
+xlabel('SNR (dB)');
+ylabel('Rad');
+title('Mean of phase estimate errors with tuned estimate');
+legend('k = 10', 'k = 20', 'tuned');
+
+dims = [200 200 500 300]; % [x_pos, y_pos, x_brd, y_brd]
+set(fig6, 'renderer', 'painters', 'position', dims, 'PaperPositionMode', 'auto');
+%print('-dpng', '-r600', '/Users/johannesskaro/Documents/KYB 3.år/EDK/Project/phi_opt_err_mean_plot');
 
 
 
